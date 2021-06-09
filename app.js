f3e8d50df70022238991bd94d17c2160f35a2a65
KaiHOtw-1010 <khoaa@connect.ust.hk>
2021-06-10 04:06:19 +0800

    Add functioning 'work' page & 'about' page. Use EJS layouts.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,8 +25,8 @@ app.get('/', function(req, res) {
 
 	// This assumes there is a folder 'views' containing a file 'list.ejs'
 	// inside 'list.ejs' there is an ejs marker called 'kindOfDay'
-	res.render('list', {
-		kindOfDay: day,
+	res.render("list", {
+		listTitle: day,
     newListItems: items
 	});
 
@@ -33,11 +34,40 @@ app.get('/', function(req, res) {
 
 // when a post request is triggered
 app.post('/', function(req, res) {
-  let item = req.body.newItem;  console.log(item);
-  items.push(item);
+  console.log(req.body);
+  let item = req.body.newItem;
 
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
+
+// another route '/work' for Work List
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+/* this does not suit as we the post request is directed to "/" home route */
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   app.redirect("/work");
+// });
+
+
+
 
 app.listen(3000, function() {
 	console.log('Server started on port 3000');
