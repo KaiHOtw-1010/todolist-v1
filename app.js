ab629d48f426c64ada610f673f434442b64d5251
KaiHOtw-1010 <51895176+KaiHOtw-1010@users.noreply.github.com>
2021-06-18 02:28:50 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,71 +1,130 @@
-// jshint esversion: 6
+//jshint esversion:6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js");
-// if console log 'express, bodyParser or date', will get the
-// module exports objects
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require('lodash');
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-// In JS, declaring const does not protect things inside object or array
-// It prevents you from assigning new array or object to it
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-
-  const day = date.getDate();
-  // const day = date.getDay();
-
-	// This assumes there is a folder 'views' containing a file 'list.ejs'
-	// inside 'list.ejs' there is an ejs marker called 'kindOfDay'
-	res.render("list", {
-		listTitle: day,
-    newListItems: items
-	});
-
+mongoose.connect("mongodb+srv://admin-kai:<dbUserPassword>@cluster0.xrf4d.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+const itemsSchema = new mongoose.Schema ({
+  name: String
 });
+const Item = mongoose.model("Item", itemsSchema);
 
-// when a post request is triggered
-app.post('/', function(req, res) {
-  console.log(req.body);
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  }
-  else {
-    items.push(item);
-    res.redirect("/");
-  }
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
 });
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
 
-// another route '/work' for Work List
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
+const listSchema = new mongoose.Schema ({
+  name: String,
+  items: [itemsSchema]
+});
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if(err) console.log(err);
+        else console.log("Successfully saved default items to DB");
+      });
+      res.redirect("/");
+    }
+    else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
   });
 });
 
-app.get("/about", function(req, res) {
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err) {
+      if (!foundList) {
+        console.log("its a new list");
+        const list = new List ({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      }
+      else {
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+app.post("/", function(req, res){
+  const itemName = req.body.newItem;
+  const listName = req.body.listName;
+
+  const item = new Item({name: itemName});
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  }
+  else {
+    List.findOne({name: listName}, function(err, foundList){
+      if (!err) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      }
+    });
+  }
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(!err) {
+        res.redirect("/");
+      }
+    });
+  }
+  else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
   res.render("about");
 });
 
-/* this does not suit as we the post request is directed to "/" home route */
-// app.post("/work", function(req, res) {
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   app.redirect("/work");
-// });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-	console.log('Server started on port 3000');
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
